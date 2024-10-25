0ff5393ce136b7c992f285accf784aaed2f6f251
Kruti <21kruti@gmail.com>
2024-10-25 14:26:57 +0530

    modified:   src/components/RoleSelection.js

--- src/components/RoleSelection.js
+++ src/components/RoleSelection.js
@@ -1,6 +1,5 @@
-// src/components/RoleSelection.js
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import './RoleSelection.css';
 
 const RoleSelection = () => {
@@ -11,11 +10,24 @@ const RoleSelection = () => {
   };
 
   return (
-    <div className="form-container">
-      <h1>Log in as?</h1>
-      <button onClick={() => handleSelection('customer')}>Customer</button>
-      <button onClick={() => handleSelection('provider')}>Service Provider</button>
-    </div>
+    <>
+
+    
+      <div className="form-container">
+        <h1>Log in as?</h1>
+        <button onClick={() => handleSelection('customer')}>Customer</button>
+        <button onClick={() => handleSelection('provider')}>Service Provider</button>
+      </div>
+
+      <footer className="footer">
+        <div className="footer-links">
+          <Link to="/FAQsCustomers">FAQs for Customers</Link>
+          <Link to="/FAQsProviders">FAQs for Providers</Link>
+          <Link to="/terms">Terms of Service</Link>
+          <Link to="/privacy">Privacy Policy</Link>
+        </div>
+      </footer>
+    </>
   );
 };
 
